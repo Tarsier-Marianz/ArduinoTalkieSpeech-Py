6bc99e57690457725b6c65d2a7b6e6a5e69e16b2
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-02-16 15:47:09 +0800

    Dead Code Elimination
    removing all un-used code to make it clean and adding some comments.

--- talkie_me.py
+++ talkie_me.py
@@ -8,14 +8,13 @@ is_version3 = False
 if (sys.version_info > (3, 0)):
     # Python 3 code in this block
     is_version3 = True
-
-     
-current_dir = os.getcwd()
+    
+current_dir = os.getcwd()   #get current directory
 wav_file = "message.wav"
 fname= os.path.join(current_dir, wav_file)
 f = open(fname, "rb")
-print (fname)
-#print "{",
+
+# start of code variable declaration based from audio filename
 code ="const uint8_t sp"+ wav_file[:-4]+"[] PROGMEM ={"
 try:
     byte = f.read(1)
@@ -23,16 +22,14 @@ try:
         # Do stuff with byte.
         byte = f.read(1)
         if is_version3:
-            code = "%s0x%s," % ( code,(binascii.hexlify(byte)).decode("ascii").upper())
             #print ("%s0x%s," % ( code,(binascii.hexlify(byte)).decode("ascii").upper()))
-
+            code = "%s0x%s," % ( code,(binascii.hexlify(byte)).decode("ascii").upper())
         else:
             #print "0x"+(binascii.hexlify(byte))+" ,",
             code = code + "0x"+(binascii.hexlify(byte)) +","
                 
 except Exception as ex:
     print (ex)
-
 finally:
     f.close()
     code = code[:-4]
